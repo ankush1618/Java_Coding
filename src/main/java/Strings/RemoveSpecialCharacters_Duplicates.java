26d694b07b7e13cd2275ac52d794f3d2be515ad6
ankush1618 <ankush.pal310@gmail.com>
2025-03-31 19:57:00 +0530

    Remove special Characters and Duplicates

--- src/main/java/Strings/RemoveSpecialCharacters_Duplicates.java
+++ src/main/java/Strings/RemoveSpecialCharacters_Duplicates.java
@@ -1,5 +1,29 @@
 package Strings;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class RemoveSpecialCharacters_Duplicates {
 
+
+    public static String removeSpecial_Duplicates(String str){
+            String newStr=str.replaceAll("[^0-9a-zA-Z]","");
+            System.out.println("String after removing special Characters :"+newStr);
+            Set<Character> set=new LinkedHashSet<>();
+            StringBuilder sb=new StringBuilder();
+            for(char ch:newStr.toCharArray()){
+                if(set.add(ch)){
+                    sb.append(ch);
+                }
+            }
+            return sb.toString();
+    }
+
+
+    public static void main(String[] args) {
+        String str="Hel@#$%l^&*/00oo";
+        System.out.println("Original String :"+str);
+        String newStr=removeSpecial_Duplicates(str);
+        System.out.println("String after removing Duplicates :"+newStr);
+    }
 }
